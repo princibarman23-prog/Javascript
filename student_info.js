4fa5bce7a290c84d85203c89f84424326e88c3c7
Princi Barman <princibarman23@gmail.com>
2026-09-22 22:45:15 +0545

    Added student info

---
+++ student_info.js
@@ -0,0 +1,16 @@
+const studentName="Princi Barman"
+const birthYear=2007
+
+let age=19
+let marks=86
+
+var course="Web Development"
+var collegeName="SITM"
+
+console.log("--- Student Values ---")
+console.log("Name:", studentName)
+console.log("Birth Year:", birthYear)
+console.log("Age:" ,age)
+console.log("M0arks:", marks)
+console.log("Course:", course)
+console.log("College:", collegeName)
