f512937298b2a1b2ed703891a5c48215fa2fc85e
Princi Barman <princibarman23@gmail.com>
2026-09-23 00:00:11 +0545

    Finding numbers

---
+++ find_num.js
@@ -0,0 +1,21 @@
+function findNumber(numbers,target){
+    let result="Not Found";
+
+    for(let i=0;i<numbers.lemgth;i++){
+        let currentNum = numbers[i];
+
+        if (currentNum < 0)
+            continue;
+
+        if (currentNum === target){
+            result = "Found";
+            break;
+        }
+    }
+
+}
+return result;
+
+const numArr=[-5,10,-2,7,20];
+console .log("Searching for 7:", findNumber(numArr,7));
+console.log("Searching for 99:", findNumber(numArr,99));
