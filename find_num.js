c29b199bf5037b142febece3f95fa75fe8cab536
Princi Barman <princibarman23@gmail.com>
2026-09-23 19:36:30 +0545

    Fixing bugs

--- find_num.js
+++ find_num.js
@@ -1,7 +1,7 @@
 function findNumber(numbers,target){
     let result="Not Found";
 
-    for(let i=0;i<numbers.lemgth;i++){
+    for(let i=0;i<numbers.length;i++){
         let currentNum = numbers[i];
 
         if (currentNum < 0)
@@ -12,10 +12,8 @@ function findNumber(numbers,target){
             break;
         }
     }
-
+    return result;
 }
-return result;
-
 const numArr=[-5,10,-2,7,20];
 console .log("Searching for 7:", findNumber(numArr,7));
 console.log("Searching for 99:", findNumber(numArr,99));
