0339ca884ed5a7f59d78af33aced48aae9eb7b7b
Princi Barman <princibarman23@gmail.com>
2026-09-23 20:38:48 +0545

    Days

---
+++ days.js
@@ -0,0 +1,20 @@
+function getDayName(day){
+    switch(day){
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        default:
+            return "Invalid Day Number";
+    }
+}
+console.log(getDayName(4));
+console.log(getDayName(9));
