0e53c30c0bea0e86a9f1f49e68a2718fbd409f3b
Princi Barman <princibarman23@gmail.com>
2026-09-26 10:02:24 +0545

    Largest element

---
+++ lar_in_Array.js
@@ -0,0 +1,9 @@
+let numbers=[12,45,7,89,23];
+
+let largest=numbers[0];
+for(let i =1;i<numbers.length;i++){
+    if (numbers[i]>largest)
+        largest=numbers[i];
+}
+
+console.log("Largest in the array is:",largest);
